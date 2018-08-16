74b7a01092467f5dbd698f5ba40183f07a316794
mengyingL <liyq@cread.com>
2018-08-16 13:59:00 +0800

    第一次提交

---
+++ app/src/main/java/com/example/administrator/gittest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //我是第二次
+        //git 第一次
+    }
+}
